b75f9bdeedbb36e00e7e612b3ad5aacf780a6682
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-21 16:33:17 +0000

    add a manifest to set up a config to forward stuff to logstash

---
+++ manifests/logstash.pp
@@ -0,0 +1,40 @@
+# == Class: rsyslog::logstash
+#
+# Full description of class role here.
+#
+# === Parameters
+#
+# [*spool_size*]
+# [*remote_type*]
+# [*custom_config*]
+# [*server*]
+# [*port*]
+#
+# === Variables
+#
+# === Examples
+#
+#  class { 'rsyslog::logstash': }
+#
+class rsyslog::logstash (
+  $spool_size        = '1g',
+  $remote_type       = 'tcp',
+  $custom_config     = undef,
+  $server            = 'localhost',
+  $port              = '5544'
+) inherits rsyslog {
+
+  $content_real = $custom_config ? {
+    ''      => template("${module_name}/34-logstash.conf.erb"),
+    default => template($custom_config),
+  }
+
+  file { $rsyslog::params::logstash_conf:
+    ensure  => present,
+    owner   => root,
+    group   => $rsyslog::params::run_group,
+    content => $content_real,
+    require => Class['rsyslog::config'],
+    notify  => Class['rsyslog::service'],
+  }
+}
