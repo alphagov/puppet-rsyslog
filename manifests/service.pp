c89fd61a1d1060bc2c0c632944792091de81e572
Garima Singh <igarimasingh@gmail.com>
2013-03-18 10:25:02 +0000

    [#46259347] Removed enable from rsyslog service
    It is currently causing errors in puppet because of
    puppet(2.7.X)/ruby(1.9.3) compatibility issues

--- manifests/service.pp
+++ manifests/service.pp
@@ -14,7 +14,6 @@ class rsyslog::service inherits rsyslog::params {
 
   service { $rsyslog::params::service_name:
     ensure  => running,
-    enable  => true,
     require => Class['rsyslog::config'],
   }
 
