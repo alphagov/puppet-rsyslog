1fc165e73cc65aa757064f5d3a025aa545622cf2
Dan Carley <dan.carley@gmail.com>
2013-04-10 09:11:45 +0100

    Pass $::lsbdistmajrelease to RedHat tests
    The addition of EL5 support in ae6427a added a nested case statement for the
    `lsbdistmajrelease` fact when `$::osfamily == 'RedHat'`. Not passing this
    fact in during spec tests caused no variables to be set and tests for RH
    were failing.

--- spec/defines/rsyslog_imfile_spec.rb
+++ spec/defines/rsyslog_imfile_spec.rb
@@ -4,7 +4,8 @@ describe 'rsyslog::imfile', :type => :define do
   context "osfamily = RedHat" do
     let :facts do
       {
-        :osfamily        => 'RedHat',
+        :osfamily          => 'RedHat',
+        :lsbmajdistrelease => 6,
       }
     end
 
